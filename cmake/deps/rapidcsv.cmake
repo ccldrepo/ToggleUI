b53cc9b9d4cdd7b0cfb0917eabc7c6225c699505
ccld <161834186+ccldrepo@users.noreply.github.com>
2024-03-13 22:08:03 +0800

    Switch to find extern lib

--- cmake/deps/rapidcsv.cmake
+++
@@ -1,7 +0,0 @@
-# https://github.com/d99kris/rapidcsv
-FetchContent_Declare(
-    rapidcsv
-    URL      "${VENDOR_GITHUB}/d99kris/rapidcsv/archive/refs/tags/v8.80.tar.gz"
-    URL_HASH SHA256=4c9e01cb2554cc76acac61532ef33b59e5b1f822160d2eb7efee2c128ea7f4c5
-    OVERRIDE_FIND_PACKAGE
-)
