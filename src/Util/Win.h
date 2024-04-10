a3b9790f5e81b799f5eb674799ee0d62b1cd5647
ccld <161834186+ccldrepo@users.noreply.github.com>
2024-04-10 21:09:03 +0800

    Update

--- src/Util/Win.h
+++ src/Util/Win.h
@@ -7,12 +7,10 @@
 #include <string_view>
 #include <type_traits>
 
-template <class T>
-concept FuncPtr = std::is_pointer_v<T> && std::is_function_v<std::remove_pointer_t<T>>;
-
 void* _GetModuleFunc(const wchar_t* a_moduleName, const char* a_funcName) noexcept;
 
-template <FuncPtr T>
+template <class T>
+    requires(std::is_pointer_v<T> && std::is_function_v<std::remove_pointer_t<T>>)
 inline T GetModuleFunc(const wchar_t* a_moduleName, const char* a_funcName) noexcept
 {
     return reinterpret_cast<T>(_GetModuleFunc(a_moduleName, a_funcName));
