92d9705b300f9ac5df08bc3e93e475041eef1288
ccld <161834186+ccldrepo@users.noreply.github.com>
2024-04-16 21:00:09 +0800

    Update PCH.h

--- src/PCH.h
+++ src/PCH.h
@@ -4,11 +4,14 @@
 #include <array>
 #include <atomic>
 #include <bit>
+#include <cctype>
 #include <chrono>
 #include <climits>
+#include <compare>
 #include <concepts>
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
 #include <exception>
 #include <filesystem>
 #include <format>
@@ -16,10 +19,13 @@
 #include <functional>
 #include <initializer_list>
 #include <ios>
+#include <map>
 #include <memory>
 #include <mutex>
 #include <optional>
 #include <ranges>
+#include <set>
+#include <shared_mutex>
 #include <span>
 #include <sstream>
 #include <stdexcept>
@@ -29,6 +35,8 @@
 #include <thread>
 #include <tuple>
 #include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -43,3 +51,13 @@
 #include <fmt/ranges.h>
 
 using namespace std::literals::string_view_literals;
+
+namespace SKSE::stl
+{
+    template <class T>
+    inline void write_thunk_call(REL::Relocation<std::uintptr_t> a_src)
+    {
+        SKSE::AllocTrampoline(14);
+        T::func = a_src.write_call<5>(T::tunk);
+    }
+}
